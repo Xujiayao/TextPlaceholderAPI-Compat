ca1809a46ed95dfbfd1a15cdba08689d80a4919e
Xujiayao <i@xujiayao.com>
2025-02-24 21:19:58 +0800

    重写进度8

--- src/main/java/eu/pb4/placeholders/impl/color/OkLch.java
+++ src/main/java/eu/pb4/placeholders/impl/color/OkLch.java
@@ -1,30 +1,30 @@
 package eu.pb4.placeholders.impl.color;
 
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.Mth;
 
 // https://bottosson.github.io/posts/oklab/
 public record OkLch(float l, float c, float h) {
 	public static OkLch fromRgb(int rgb) {
-		var lab = OkLab.fromRgb(rgb);
-		var c = MathHelper.sqrt(lab.a() * lab.a() + lab.b() + lab.b());
-		var h = (float) MathHelper.atan2(lab.b(), lab.a());
+		OkLab lab = OkLab.fromRgb(rgb);
+		float c = Mth.sqrt(lab.a() * lab.a() + lab.b() + lab.b());
+		float h = (float) Mth.atan2(lab.b(), lab.a());
 
 		return new OkLch(lab.l(), c, h);
 	}
 
 	public static int toRgb(float l, float c, float h) {
-		return OkLab.toRgb(l, (float) (c * Math.cos(h)), (float) (c * Math.sin(h)));
+		return OkLab.toRgb(l, (float) ((double) c * Math.cos(h)), (float) ((double) c * Math.sin(h)));
 	}
 
 	public float a() {
-		return c * MathHelper.cos(h);
+		return this.c * Mth.cos(this.h);
 	}
 
 	public float b() {
-		return c * MathHelper.sin(h);
+		return this.c * Mth.sin(this.h);
 	}
 
 	public int toRgb() {
-		return OkLab.toRgb(l, this.a(), this.b());
+		return OkLab.toRgb(this.l, this.a(), this.b());
 	}
 }
--- src/main/java/eu/pb4/placeholders/impl/placeholder/PlaceholderNode.java
+++ src/main/java/eu/pb4/placeholders/impl/placeholder/PlaceholderNode.java
@@ -2,33 +2,34 @@ package eu.pb4.placeholders.impl.placeholder;
 
 import eu.pb4.placeholders.api.ParserContext;
 import eu.pb4.placeholders.api.PlaceholderContext;
+import eu.pb4.placeholders.api.PlaceholderHandler;
 import eu.pb4.placeholders.api.Placeholders;
 import eu.pb4.placeholders.api.node.TextNode;
 import eu.pb4.placeholders.impl.GeneralUtils;
-import net.minecraft.text.Text;
-import org.jetbrains.annotations.ApiStatus;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.Nullable;
 
-@ApiStatus.Internal
+@Internal
 public record PlaceholderNode(ParserContext.Key<PlaceholderContext> contextKey, String placeholder,
                               Placeholders.PlaceholderGetter getter, boolean optionalContext,
                               @Nullable String argument) implements TextNode {
 	@Override
-	public Text toText(ParserContext context, boolean removeBackslashes) {
-		var ctx = context.get(contextKey);
-		var handler = getter.getPlaceholder(placeholder, context);
+	public Component toText(ParserContext context, boolean removeBackslashes) {
+		PlaceholderContext ctx = context.get(this.contextKey);
+		PlaceholderHandler handler = this.getter.getPlaceholder(this.placeholder, context);
 		if ((ctx != null || this.optionalContext) && handler != null) {
 			try {
-				return handler.onPlaceholderRequest(ctx, argument).text();
+				return handler.onPlaceholderRequest(ctx, this.argument).text();
 			} catch (Throwable e) {
-				GeneralUtils.LOGGER.error("Error occurred while parsing placeholder " + placeholder + " / " + contextKey.key() + "!", e);
-				return Text.empty();
+				GeneralUtils.LOGGER.error("Error occurred while parsing placeholder " + this.placeholder + " / " + this.contextKey.key() + "!", e);
+				return Component.empty();
 			}
 		} else {
 			if (GeneralUtils.IS_DEV) {
-				GeneralUtils.LOGGER.error("Missing context for placeholders requiring them (" + placeholder + " / " + contextKey.key() + ")!", new NullPointerException());
+				GeneralUtils.LOGGER.error("Missing context for placeholders requiring them (" + this.placeholder + " / " + this.contextKey.key() + ")!", new NullPointerException());
 			}
-			return Text.empty();
+			return Component.empty();
 		}
 	}
 
